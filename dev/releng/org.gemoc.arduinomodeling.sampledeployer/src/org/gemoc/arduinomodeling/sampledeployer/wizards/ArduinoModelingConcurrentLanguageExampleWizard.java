17f18feb0135e919cf29515d89187828b89f457f
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-03-09 16:57:23 +0100

    deployment of Arduino concurrent in the discovery

--- dev/releng/org.gemoc.arduinomodeling.sampledeployer/src/org/gemoc/arduinomodeling/sampledeployer/wizards/ArduinoModelingConcurrentLanguageExampleWizard.java
+++ dev/releng/org.gemoc.arduinomodeling.sampledeployer/src/org/gemoc/arduinomodeling/sampledeployer/wizards/ArduinoModelingConcurrentLanguageExampleWizard.java
@@ -19,15 +19,23 @@ import org.gemoc.arduinomodeling.sampledeployer.Activator;
 
 import fr.inria.diverse.commons.eclipse.jface.wizards.AbstractExampleWizard;
 
-public class ArduinoModelingModelExampleWizard
+public class ArduinoModelingConcurrentLanguageExampleWizard
 	extends AbstractExampleWizard {
 	
 	protected Collection<ProjectDescriptor> getProjectDescriptors() {		
 		// We need the statements example to be unzipped along with the
 		// EMF library example model, edit and editor examples
 		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(1);
-		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/ifdemo.zip", "ifdemo"));
-		//projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.sigpml.simple_example_with_melange.zip", "org.gemoc.sample.sigpml.simple_example_with_melange"));
+		
+		// sequential projects
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent", 						"org.gemoc.arduino.concurrent"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent.design.zip", 			"org.gemoc.arduino.concurrent.design"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent.ecl.zip", 				"org.gemoc.arduino.concurrent.ecl"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent.execarduino.zip", 		"org.gemoc.arduino.concurrent.execarduino"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent.execarduino.edit.zip", 	"org.gemoc.arduino.concurrent.execarduino.edit"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent.execarduino.editor.zip", "org.gemoc.arduino.concurrent.execarduino.editor"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.arduino.concurrent.operationalsemantics.zip", "org.gemoc.arduino.concurrent.operationalsemantics"));
+
 		return projects;
 	}
 
